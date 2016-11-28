7f4c300cac8938640705c58be297d8cd49679313
yurchik <15_yurchik@ukr.net>
2016-11-28 10:52:43 +0200

    Add IPv4 analys

--- sniffer.py
+++ sniffer.py
@@ -2,6 +2,16 @@ import socket
 import struct
 import textwrap
 
+TAB_1 = '\t -  '
+TAB_2 = '\t\t -  '
+TAB_3 = '\t\t\t -  '
+TAB_4 = '\t\t\t\t -  '
+
+DATA_TAB_1 = '\t  '
+DATA_TAB_2 = '\t\t  '
+DATA_TAB_3 = '\t\t\t  '
+DATA_TAB_4 = '\t\t\t\t  '
+
 
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
@@ -9,7 +19,7 @@ def main():
     while True:
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-        print("Ethernet Frame:")
+        print("\nEthernet Frame:")
         print("Destination: {} Source: {} Proto: {}".format(dest_mac, src_mac, eth_proto))
 
 
@@ -23,6 +33,20 @@ def ethernet_frame(data):
 def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
+
+
+# Unpack IPv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+
+# Return properly formatted IPv4 address
+def ipv4(address):
+    return '.'.join(map(str, address))
 
 
 main()
