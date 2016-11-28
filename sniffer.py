3d16f10737904aaab60790faf1658c8db23d3bac
yurchik <15_yurchik@ukr.net>
2016-11-28 16:52:50 +0200

    Add UDP out, print multiline information and out all information about every packeg

--- sniffer.py
+++ sniffer.py
@@ -20,13 +20,50 @@ def main():
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print("\nEthernet Frame:")
-        print("Destination: {} Source: {} Proto: {}".format(dest_mac, src_mac, eth_proto))
+        print(TAB_1 + "Destination: {} Source: {} Proto: {}".format(dest_mac, src_mac, eth_proto))
+
+        # 8 for IPv4
+        if eth_proto == 8:
+            (version, header_length, ttl, proto, src, target, data) = ipv4_packet(data)
+            print(TAB_1 + 'IPv4 packet:')
+            print(TAB_2 + 'Version: {} Header Length: {} TTL: {} '.format(version, header_length, ttl))
+            print(TAB_2 + 'Protocol: {} Source: {} Target: {} '.format(proto, src, target))
+
+            # ICMP
+            if proto == 1:
+                icmp_type, code, checksum, data = icmp_packet(data)
+                print(TAB_1 + 'ICMP Packet:')
+                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {}, '.format(icmp_type, code, checksum))
+                print(TAB_2 + 'Data: {}')
+                print(format_multi_line(DATA_TAB_3, data))
+
+            # TCP
+            elif proto == 6:
+                (src_port, dest_port, sequence, acknowledge, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data) = tcp_packet(data)
+                print(TAB_1 + 'TCP Segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(src_port, dest_port))
+                print(TAB_2 + 'Sequence: {}, Acknowledge: {}'.format(sequence, acknowledge))
+                print(TAB_2 + 'Flags:')
+                print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin))
+                print(TAB_2 + 'Data: {}')
+                print(format_multi_line(DATA_TAB_3, data))
+
+            # UDP
+            elif proto == 17:
+                src_port, dest_port, length, data = udp_packet(data)
+                print(TAB_1 + 'UDP segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(src_port, dest_port, length))
+
+            # Other
+            else:
+                print(TAB_1 + 'Data:')
+                print(format_multi_line(DATA_TAB_2, data))
 
 
-# Unpakc Ethernet data
+# Unpack Ethernet data
 def ethernet_frame(data):
     mac_dest, mac_src, proto = struct.unpack('! 6s 6s H', data[:14])
-    return get_mac_addr(mac_dest), get_mac_addr(mac_src), socket.htons(proto), data[:14]
+    return get_mac_addr(mac_dest), get_mac_addr(mac_src), socket.htons(proto), data[14:]
 
 
 # Return properly formating mac
